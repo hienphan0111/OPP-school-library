66e4d09312e910823003e21bfcb82142e2b2ad64
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-04-17 21:31:31 +0700

    Add Person, Student as well as teacher classes

---
+++ student.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+
+class Student < Person
+
+  attr_accessor :classroom
+
+  def initialize(id, age, classroom, name = 'Unknow')
+    super(id, age, name)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
