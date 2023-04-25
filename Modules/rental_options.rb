d94675829fd35cdf04ae11aaff91a3287a8558d2
Menekam Rudy <menekamrudy@gmail.com>
2023-04-25 16:39:15 +0100

    Store rentals data

--- Modules/rental_options.rb
+++ Modules/rental_options.rb
@@ -1,13 +1,16 @@
 require_relative '../Components/rental'
 require_relative 'input'
+require_relative '../utils/io_file'
 
 class RentalOptions
   attr_accessor :rentals_list
 
   def initialize(book_options, people_options)
-    @rentals_list = []
     @book_options = book_options
     @people_options = people_options
+    @rental_file = IOFile.new('./data/rentals.json')
+    @rental_data = @rental_file.read_data
+    @rentals_list = @rental_data.map { |data| Rental.new(data['date'], data['book'], data['person']) }
   end
 
   def select(label, option)
@@ -27,6 +30,9 @@ class RentalOptions
 
     date = Input.user_input('Date: ')
     @rentals_list.push(Rental.new(date, @book_options.books_list[book_num], @people_options.people_list[person_num]))
+    @rental_data << { 'date' => date, 'book' => @book_options.books_list[book_num],
+                      'person' => @people_options.people_list[person_num] }
+    @rental_file.write_data(@rental_data)
     puts 'Rental created successfully'
   end
 
